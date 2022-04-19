e4cb0980c217614a7ec800f154d1fc99ae56ad0f
danielRojas14 <danielrojas131415@gmail.com>
2022-04-19 16:32:36 -0300

    primer commit

---
+++ index.js
@@ -0,0 +1,58 @@
+const img = document.getElementById("img");
+
+// Load the model.
+const predicciones = cocoSsd.load().then((model) => {
+  // detect objects in the image.
+  model.detect(img).then((predictions) => {
+    const divMensaje = document.getElementById("mensaje");
+    const { class: clase } = predictions[0];
+    divMensaje.className = "alert alert-success text-center";
+    divMensaje.style = "width: 600px";
+      divMensaje.innerHTML = `Prediccion: <b>${clase}</b>`;
+      
+      return clase;
+  });
+    
+});
+
+function myFunction(paramPrediccion) {
+  var voices = speechSynthesis.getVoices();
+  //console.log(voices);
+  if (voices.length !== 0) {
+    var msg = new SpeechSynthesisUtterance();
+
+    msg.text = paramPrediccion;
+    msg.lang = document.getElementById("lang").value; //'hi-IN';
+
+    const select = document.getElementById("lang");
+
+    for (var i = 0; i < voices.length; i++) {
+      var opcion = document.createElement("option");
+      opcion.text = voices[i].name + " (" + voices[i].lang + ")";
+      select.add(opcion);
+
+      // if (voices[i].lang == msg.lang) {
+      //   msg.voice = voices[i]; // Note: some voices don't support altering params
+      //   msg.voiceURI = voices[i].voiceURI;
+      //   // break;
+      // }
+    }
+
+    msg.onend = function (e) {
+      console.log("Finished in " + event.elapsedTime + " seconds.");
+    };
+
+    speechSynthesis.speak(msg);
+
+    clearInterval(mytimer);
+  }
+}
+
+predicciones.then((clase) => {
+
+
+
+    document.getElementById("boton").addEventListener("click", function () {
+        myFunction(clase);
+    });
+}
