af19ec3d17be984909a2c04e0551cf6bd5b6f560
danielRojas14 <danielrojas131415@gmail.com>
2022-04-20 16:43:27 -0300

    primer commit

--- index.js
+++ index.js
@@ -2,62 +2,8 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
-const translate = require("@iamtraction/google-translate");
-const cocoSsd = require("@tensorflow-models/coco-ssd");
-// const img = document.getElementById("img");
+const path = require("path");
 
-const cargarVoces = () => {
-  var voices = speechSynthesis.getVoices();
-
-  const select = document.getElementById("lang");
-
-  for (var i = 0; i < voices.length; i++) {
-    var opcion = document.createElement("option");
-    opcion.text = voices[i].name + " (" + voices[i].lang + ")";
-    select.add(opcion);
-  }
-};
-const reproducirPrediccion = (paramPrediccion) => {
-  var msg = new SpeechSynthesisUtterance();
-
-  msg.text = paramPrediccion;
-  msg.lang = document.getElementById("lang").value;
-
-  speechSynthesis.speak(msg);
-
-  msg.onend = function (e) {
-    console.log("Finished in " + event.elapsedTime + " seconds.");
-  };
-};
-
-translate("Tu es incroyable!", { to: "en" })
-  .then((res) => {
-    console.log(res.text); // OUTPUT: You are amazing!
-  })
-  .catch((err) => {
-    console.error(err);
-  });
-
-// // Load the model.
-// const predicciones = cocoSsd.load().then((model) => {
-//   // detect objects in the image.
-//   model.detect(img).then((predictions) => {
-//     console.log(predictions);
-//     const divMensaje = document.getElementById("mensaje");
-//     const { class: clase } = predictions[0];
-//     divMensaje.className = "alert alert-success text-center";
-//     divMensaje.style = "width: 600px";
-//     divMensaje.innerHTML = `Prediccion: <b>${clase}</b>`;
-
-//     const select = document.getElementById("lang");
-
-//     document.getElementById("lang").addEventListener("click", function () {
-//       cargarVoces();
-//     });
-
-//     document.getElementById("boton").addEventListener("click", () => {});
-//   });
-// });
+app.use("/", express.static(path.join(__dirname, "public")));
